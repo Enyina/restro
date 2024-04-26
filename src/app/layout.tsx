e356b3d26d9b5cc701e4dd5dcbaa5ac492f650e7
Enyina <enyina3848@gmail.com>
2024-04-26 12:17:32 +0100

    update: ui fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='inter.className'>
-        <div  className={`bg-default_background_color m-1  rounded-3xl p-0 min-h-screen md:mx-24 lg:mx-48 xl:mx-96 no-scrollbar `}>
+        <div  className={`bg-default_background_color   rounded-3xl min-h-screen md:mx-24 lg:mx-48 xl:mx-96 no-scrollbar `}>
 <Providers>
         {children}
 
